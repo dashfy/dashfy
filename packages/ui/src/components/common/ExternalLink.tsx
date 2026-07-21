ade5d4ceb345a2561dd09b65b3851cce51833c3f
brenopolanski <breno.polanski@gmail.com>
2026-07-21 11:17:17 -0300

    refactor(ExternalLink): add default rel and target props for links

--- packages/ui/src/components/common/ExternalLink.tsx
+++ packages/ui/src/components/common/ExternalLink.tsx
@@ -2,17 +2,20 @@ import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
-export interface ExternalLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-  href: string
-}
-
-export const ExternalLink = ({ children, className, href, ...props }: ExternalLinkProps) => {
+export const ExternalLink = ({
+  children,
+  className,
+  href,
+  rel = 'noopener noreferrer',
+  target = '_blank',
+  ...props
+}: React.ComponentProps<'a'>) => {
   return (
     <a
       className={cn('text-primary transition-colors hover:underline', className)}
       href={href}
-      rel="noopener noreferrer"
-      target="_blank"
+      rel={rel}
+      target={target}
       {...props}
     >
       {children}
